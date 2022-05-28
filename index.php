d73f51f4d80c22cc659ee6e73b0851b72edae1ec
devbyanuwat <mailfordevbyanuwat@gmail.com>
2022-05-28 15:29:42 +0700

    change text del button

--- index.php
+++ index.php
@@ -100,13 +100,13 @@
 
                 function conf_del(id) {
                     Swal.fire({
-                        title: 'Are you sure?',
-                        text: "You won't be able to revert this!",
+                        title: 'ยืนยันการลบหรือไม่?',
                         icon: 'warning',
                         showCancelButton: true,
+                        cancelButtonText: "ยกเลิก",
                         confirmButtonColor: '#3085d6',
                         cancelButtonColor: '#d33',
-                        confirmButtonText: 'Yes, delete it!',
+                        confirmButtonText: 'ยืนยันการลบ',
 
                     }).then((result) => {
                         if (result.isConfirmed) {
